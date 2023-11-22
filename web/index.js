8ec6fd0f094fc9b9568d06306660f59b8705ed21
mitsuji <tkms@mitsuji.org>
2023-11-22 17:24:24 +0900

    mod: OK

--- web/index.js
+++ web/index.js
@@ -1,29 +1,91 @@
+// * Promise イベントハンドラベースのコード群を逐次的に記述するための道具
+//   * コールバック地獄対策
+//   * 計算結果を次の処理に渡す
+//   * 例外をコード群全体の catch ブロックになげる
+//
+// * async/await Promise を組み合わせたコード群を通常のコードのように記述するための道具
+//   * async Promise の 自動作成
+//     * return -> resolveの呼び出し
+//     * throw -> rejectの呼び出し
+//   * Promiseの呼び出し は await をつけると 自動的に .then
+//   * Promise以外は 自動的に .then
+//   * await に対する try/catch .catch
+
+
+function foo (p) {
+    return new Promise((resolve,reject) => {
+//        reject("foo");
+        resolve(p + 2);
+    });
+}
+function bar (p) {
+    return new Promise((resolve,reject) => {
+//        reject("bar");
+        resolve(p * 3);
+    });
+}
+
+
+function baz1 () {
+    return new Promise((resolve,reject) => {
+        resolve(1);
+    })
+        .then(foo)
+        .then(bar)
+}
+
+async function baz2 () {
+    let r1 = await 1;
+//    throw "baz1";
+    let r2 = await foo(r1);
+//    throw "baz2";
+    let r3 = await bar(r2);
+//    throw "baz3";
+    return r3;
+}
+
+
 
 window.onload = (e) => {
-    document.querySelector(".sendbutton").addEventListener("click",(event) => {
-        console.log("送信");
+    document.querySelector(".sendbutton").addEventListener("click",async(event) => {
+        baz1()
+            .then((result) => {
+                console.log("result:" + result);
+            })
+            .catch((error) => {
+                console.log("error:" + error);
+            });
+//        try {
+//            let result = await baz2();
+//            console.log("result:" + result);
+//        } catch (error) {
+//            console.log("error:" + error);
+//        }
+    });
+//    document.querySelector(".sendbutton").addEventListener("click",(event) => {
+//        console.log("送信");
+////        {
+////            document.querySelector(".content1").innerHTML = "Content1!!";
+////            let req = new XMLHttpRequest();
+////            req.open("GET", "/get1", false);
+////            req.send();
+////            document.querySelector(".content2").innerHTML = req.responseText;
+////            document.querySelector(".content3").innerHTML = "Content3!!";
+////        }
 //        {
 //            document.querySelector(".content1").innerHTML = "Content1!!";
 //            let req = new XMLHttpRequest();
-//            req.open("GET", "/get1", false);
+//            req.open("GET", "/get1");
+//            req.onload = (event) => {
+//                try {
+//                    document.querySelector(".content2").innerHTML = req.responseText;
+//                    throw "error1";
+//                } catch (error) {
+//                    console.log("error:" + error);
+//                }
+//            };
 //            req.send();
-//            document.querySelector(".content2").innerHTML = req.responseText;
 //            document.querySelector(".content3").innerHTML = "Content3!!";
 //        }
-        {
-            document.querySelector(".content1").innerHTML = "Content1!!";
-            let req = new XMLHttpRequest();
-            req.open("GET", "/get1");
-            req.onload = (event) => {
-                try {
-                    document.querySelector(".content2").innerHTML = req.responseText;
-                    throw "error1";
-                } catch (error) {
-                    console.log("error:" + error);
-                }
-            };
-            req.send();
-            document.querySelector(".content3").innerHTML = "Content3!!";
-        }
-    });
+//    });
 };
