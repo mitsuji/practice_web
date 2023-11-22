73c69c1d8fdb41e3e05c150cba82b49f4cc7e0eb
mitsuji <tkms@mitsuji.org>
2023-11-22 15:25:04 +0900

    mod: OK

--- web/index.js
+++ web/index.js
@@ -1,23 +1,24 @@
-//function send() {
-//    console.log("送信");
-//}
 
 window.onload = (e) => {
     document.querySelector(".sendbutton").addEventListener("click",(event) => {
         console.log("送信");
 //        {
+//            document.querySelector(".content1").innerHTML = "Content1!!";
 //            let req = new XMLHttpRequest();
 //            req.open("GET", "/get1", false);
 //            req.send();
-//            console.log(req.responseText);
+//            document.querySelector(".content2").innerHTML = req.responseText;
+//            document.querySelector(".content3").innerHTML = "Content3!!";
 //        }
         {
+            document.querySelector(".content1").innerHTML = "Content1!!";
             let req = new XMLHttpRequest();
             req.open("GET", "/get1");
             req.onload = (event) => {
-                console.log(req.responseText);
+                document.querySelector(".content2").innerHTML = req.responseText;
             };
             req.send();
+            document.querySelector(".content3").innerHTML = "Content3!!";
         }
     });
 };
